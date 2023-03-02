9cdd583eedc4882d40c6fb32ef5355d41a6ca1b6
Ronaldo Vargas <ronaldovh99@gmail.com>
2023-03-01 19:31:27 -0500

    Initial Commit

---
+++ SwiftUI-Titans-Crypto/SwiftUI_Titans_CryptoApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUI_Titans_CryptoApp.swift
+//  SwiftUI-Titans-Crypto
+//
+//  Created by Ronaldo on 1/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Titans_CryptoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
